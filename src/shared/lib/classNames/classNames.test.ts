d94a08173d3ab1a138e95d563dec2187f8911041
Kirill Fedotov <rtdkirill@gmail.com>
2024-01-24 14:08:07 +0400

    fixed unit test

--- src/shared/lib/classNames/classNames.test.ts
+++ src/shared/lib/classNames/classNames.test.ts
@@ -3,6 +3,6 @@ import { classNames } from './classNames'
 describe('classNames', () => {
     test('', () => {
         expect(classNames('styles', { hovered: true }, ['class1', 'class2']))
-            .toBe('styles hovered class1 class2')
+            .toBe('styles class1 class2 hovered')
     })
 })
